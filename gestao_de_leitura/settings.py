bb5b52d2bda46944369815b31bcbbdce1648683f
Juan-Costa-Lemos <juancostask@gmail.com>
2022-09-07 19:32:38 -0300

    acrescentando os host em settings e debug

--- gestao_de_leitura/settings.py
+++ gestao_de_leitura/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-r_qnhdn^jic@%zb00pf1fupff2!ng@kfs&!=)ux&otw9zdazx1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['gestor-leitura.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
