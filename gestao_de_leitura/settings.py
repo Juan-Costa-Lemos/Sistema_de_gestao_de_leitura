b91f88956a92b82c2be891c1b63af5e0ddcf7719
Juan-Costa-Lemos <juancostask@gmail.com>
2022-09-09 18:48:08 -0300

    allowed hosts

--- gestao_de_leitura/settings.py
+++ gestao_de_leitura/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-r_qnhdn^jic@%zb00pf1fupff2!ng@kfs&!=)ux&otw9zdazx1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['gestor-leitura.herokuapp.com/','127.0.0.1']
+ALLOWED_HOSTS = ['*','127.0.0.1']
 
 
 # Application definition
